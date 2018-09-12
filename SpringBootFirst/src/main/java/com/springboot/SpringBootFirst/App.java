23806747c7123dc2f6e43cffc6ded846a981d94f
LuoFeng <>
2018-09-12 15:42:04 +0800

    测试1

--- SpringBootFirst/src/main/java/com/springboot/SpringBootFirst/App.java
+++ SpringBootFirst/src/main/java/com/springboot/SpringBootFirst/App.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * 启动类;
  * @author 
- * @version v.0.1
+ * @version v.0.1  
  */
 @SpringBootApplication
 public class App extends SpringBootServletInitializer  {
